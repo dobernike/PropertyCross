dbe1ddcb56d226c8e9ac414a446c1d3d5d125410
Paul <dobernike@mail.ru>
2019-09-12 12:53:25 +0300

    Added postCSS and CSSmodule TASK-8: added  postCSS and CSSmodule

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { Page1, Page2, Page3 } from './pages';
 import Menu from './Menu';
 
+import styles from '../styles/App.css';
+
 function App() {
     return (
         <>
             <Menu />
             <main>
                 <Switch>
-                    <Route path="/" component={() => <h1>Hello world</h1>} exact />
+                    <Route path="/" component={() => <h1 className={styles.title}>Hello world</h1>} exact />
                     <Route path="/page1" component={Page1} />
                     <Route path="/page2" component={Page2} />
                     <Route path="/page3" component={Page3} />
